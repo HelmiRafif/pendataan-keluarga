07a4cb1e5a54bf1ad2dcb57b86d480c69f13135a
HelmiRafif <rafif.vdh@gmail.com>
2021-04-08 18:19:36 +0700

    Co-authored-by: Dimas Setio <dimassetio@users.noreply.github.com>

--- resources/views/kependudukan.blade.php
+++ resources/views/kependudukan.blade.php
@@ -139,20 +139,21 @@
         <div class="text-center mx-auto py-6">
             <a href="/kb"><button class="bg-indigo-600 align-middle text-white hover:text-gray-300 font-bold py-3 px-4 rounded-full">Selanjutnya <i class="fas fa-arrow-right"></i></button></a>
         </div>
+    <!-- MODAL BOS  -->
+    <!-- <div class="modal md:w-full opacity-0 pointer-events-none overflow-y-scroll fixed w-full h-full top-0 left-0 flex items-center z-10" style="height:255%;"> -->
+    <div class="modal  overflow-y-scroll fixed w-full bg-gray-900 bg-opacity-50 h-full top-0 left-0 z-10">
 
-    <div class="modal md:w-full opacity-0 pointer-events-none overflow-y-scroll fixed w-full h-full top-0 left-0 flex items-center z-10" style="height:255%;">
-        <div class="modal-overlay absolute w-full bg-gray-900 opacity-50 overflow-y-scroll" style="height:235%; width:110%"></div>
+        <!-- <div class="modal-container bg-white w-11/12 md:max-w-3xl mx-auto rounded shadow-lg z-50 overflow-y-auto"> -->
+        <div class="modal-container bg-white w-11/12 md:max-w-3xl mx-auto my-4 rounded shadow-lg z-50 overflow-y-scroll">
         
-        <div class="modal-container bg-white w-11/12 md:max-w-3xl mx-auto rounded shadow-lg z-50 overflow-y-auto">
-        
-        <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+        <!-- <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
             <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
             <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
             </svg>
             <span class="text-sm ">Tutup</span>
-        </div>
+        </div> -->
 
-        <div class="modal-content md:w-full py-4 text-left px-6">
+        <div class="modal-content md:w-full py-4 text-left px-6 my-2">
             {!! Form::open(array('route' => 'kependudukan.store','method' => 'POST')) !!}
             {{-- Title --}}
             <div class="flex justify-between items-center pb-3">
